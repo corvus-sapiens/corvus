cd82f5e8134cf8cee5aa17fa59e8ae6907501411
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2021-10-17 02:51:28 +0300

    Turn this project into an installable package

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup for the 'corvus' package"""
+
+
+import setuptools  # type: ignore
+
+with open("VERSION", "r", encoding="utf-8") as file:
+    version = file.read().strip()
+
+with open("pip-requirements.txt", "r", encoding="utf8") as file:
+    requirements = [line.strip() for line in file if line]
+
+setuptools.setup(
+    name='corvus',
+    version=version,
+    description='An assortment of convenience tools',
+    url='#',
+    author='Alexander Gorelyshev',
+    install_requires=requirements,
+    author_email='alexander.gorelyshev@pm.me',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
