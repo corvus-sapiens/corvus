d746d0f580ede5736c3ef88f53c06c62af1ad3e5
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2021-12-11 18:10:23 +0300

    Clean up

--- corvus/cdocker.py
+++ corvus/cdocker.py
@@ -1,9 +1,15 @@
 import docker
 from docker.errors import ImageNotFound
-from requests.exceptions import HTTPError
 
 
 def images_exists(name: str, tag: str) -> bool:
+    """
+    Check if a Docker image with a given `name`:`tag` combination exists.
+
+    :param name: image name
+    :param tag: image tag (e.g., version, "latest", etc.)
+    :return: True if the image exists, false otherwise
+    """
     dclient = docker.client.from_env()
 
     try:
