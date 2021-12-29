28d52647209d05b33e66b9fa440a953a20e2fb07
Alexander Gorelyshev <alexander.gorelyshev@pm.me>
2021-12-29 16:13:50 +0300

    Add correct docstring

--- corvus/misc.py
+++ corvus/misc.py
@@ -367,6 +367,7 @@ def get_bin_version(path: str, logger: logging.LoggerAdapter) -> str:
 
 ## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ##
 def get_file_report(path: str, sample_bytes: int = 2048) -> FileReport:
+    """Gather file stats like size, creation/modification time and MIME type.
     :param path: path to be analyzed
     :param sample_bytes: sample that many bytes before guessing the MIME type
     :returns: Dict containing information about the file under the `path`
